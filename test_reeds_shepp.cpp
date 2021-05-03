17aa20d8fd6f66e8ed8a025c04858aff8927864d
Jaskaran <jaskarag@andrew.cmu.edu>
2021-05-02 21:59:35 -0400

    got a vector of paths from reedshepp

--- test_reeds_shepp.cpp
+++ test_reeds_shepp.cpp
@@ -1,6 +1,7 @@
 #include "code/reeds_shepp.h"
 #include <iostream>
 #include <chrono>
+using namespace std;
 
 
 int main(int argc, char** argv) {
@@ -8,25 +9,33 @@ int main(int argc, char** argv) {
   std::cout << "Test started!" << std::endl;
 
   double start[3] = {0,0,0};
-  double goal[3] = {10,10,0};
+  double goal[3] = {5,0,1.56};
 
   ReedsSheppStateSpace rs = ReedsSheppStateSpace(1);
 
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
   ReedsSheppStateSpace::ReedsSheppPath rspath = rs.reedsShepp(start,goal);
   
-  double interpolated_state[3];
+  // double interpolated_state[3];
 
   double r = 0;
+  double interpolated_state[3];
+  
+  vector<vector<double>> smooth_path;
   while (r < rspath.length()) {
-    rs.interpolate(start,rspath, 7, interpolated_state);
+    rs.interpolate(start,rspath, r, interpolated_state);
+    vector<double> is;
+    for (int i = 0 ; i < 3 ; i++){
+      is.push_back(interpolated_state[i]);
+    }
+    smooth_path.push_back(is);
     r += 0.1;
   }
   
-  //printf("Interpolated state: [%.2f, %.2f, %.2f]", interpolated_state[0], interpolated_state[1], interpolated_state[2]);
+  // printf("Interpolated state: [%.2f, %.2f, %.2f]", interpolated_state[0], interpolated_state[1], interpolated_state[2]);
   high_resolution_clock::time_point t2 = high_resolution_clock::now();
 
-  duration<double> time = duration_cast<duration<double>>(t2 - t1);
+  duration<double> time = duration_cast<duration<double> >(t2 - t1);
   
 // To get the value of duration use the count()
 // member function on the duration object
